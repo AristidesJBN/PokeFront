56cde6a28a00da318e50ba957de349e031d37f21
Aristides Neto <aristidesjbn@gmail.com>
2026-01-21 17:16:06 -0300

    feat: componentes e serviços

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PokemonListComponent } from './components/pokemon-list/pokemon-list';
-import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details';
-
-export const routes: Routes = [
-  { path: '', component: PokemonListComponent },
-  { path: 'pokemon/:id', component: PokemonDetailsComponent }
-];
