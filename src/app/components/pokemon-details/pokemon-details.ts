7bd85647f257927bca76c072d47035fa1de419a7
Aristides Neto <aristidesjbn@gmail.com>
2026-01-21 17:17:19 -0300

    feat: componentes e serviços

---
+++ src/app/components/pokemon-details/pokemon-details.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PokemonService } from '../../services/pokemon';
+
+@Component({
+  selector: 'app-pokemon-details',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './pokemon-details.html',
+  styleUrls: ['./pokemon-details.css']
+})
+export class PokemonDetailsComponent implements OnInit {
+
+  pokemon: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+  const id = this.route.snapshot.paramMap.get('id');
+
+  if (id) {
+    this.pokemonService.getPokemonById(id).subscribe(data => {
+      this.pokemon = data;
+    });
+  }
+}
+
+}
