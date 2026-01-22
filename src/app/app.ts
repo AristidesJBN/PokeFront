235c08778b2ce79f8a96f645808e32693de3dcb3
Aristides Neto <aristidesjbn@gmail.com>
2026-01-22 13:56:55 -0300

    refactor: ajustes no arquivo principal da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {}
